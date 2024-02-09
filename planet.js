60389b0e09181806595787e13b1aad1b0a69e1db
MarieG41 <mariegond8@gmail.com>
2024-02-09 15:43:49 +0100

    infos planets displayed

--- planet.js
+++ planet.js
@@ -8,7 +8,6 @@ function planetName() {
                 .then((reponse) => reponse.json())
                 .then((reponse) => {
                     planetsAll = planetsAll.concat(reponse.results);
-                    //console.log(planetsAll);
                     planetsDisplay(reponse.results); 
                 }) 
     }
@@ -20,14 +19,29 @@ function planetsDisplay(planetes) {
         const items = document.createElement('li');
         items.textContent = planete.name;
         starPlanets.appendChild(items);
-        items.addEventListener('click', function () {
-            console.log('cliqué');
-        })
+        items.addEventListener('click', function (event) {
+            displayPlanetInfos(planetsAll, event);
+        });
     });
-};
+}
 
-function displayPlanetInfos() {
-    
+function displayPlanetInfos(planetsAll, event) {
+    const planetName = event.target.textContent
+    //console.log(planetName);
+    const planet = planetsAll.find(planet => planetName == planet.name);
+    console.log(planet);
+    document.querySelector('.planet-name').textContent = planet.name; 
+
+    populationInfo = document.querySelector('.population').textContent = `Population : ${planet.population}`;
+
+    gravityInfo = document.querySelector('.gravity').textContent = `Gravité : ${planet.gravity}`;
+
+    diameterInfo = document.querySelector('.diameter').textContent = `Diamètre : ${planet.diameter}`;
+
+    diameterInfo = document.querySelector('.climate').textContent = `Climat : ${planet.climate}`;
+
+    diameterInfo = document.querySelector('.terrain').textContent = `Terrain: ${planet.terrain}`;
+
 }
 // function getCountPage() {
 //     const countPages = reponse.next;
